53129dcca3d9b086199ae1a9bda311d7d58aaff5
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2024-02-28 18:43:53 +0400

    less_02

---
+++ less_02/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Comments from './components/CommentsList/CommentsList'
+
+function App() {
+  return (
+    <div className="App">
+      <Comments />
+    </div>
+  );
+}
+
+export default App;
